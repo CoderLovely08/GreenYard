9bb305a1344760bc71970094036b7779d3e6fe07
CoderLovely08 <lovelysingeras297@gmail.com>
2022-11-21 19:27:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get("/",function(req,res){
+    res.send("Hello!");
+});
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000!");
+});
