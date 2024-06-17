d2330e0c9b708aa88f5f9a566ca13b60d6c1a73f
Lovely Sharma <lovelysingeras297@gmail.com>
2024-06-17 23:26:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -432,6 +432,6 @@ app.use((req, res, next) => {
 });
 
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000!");
+app.listen(process.env.PORT, function () {
+    console.log(`Server is running on port ${process.env.PORT}!`);
 });
